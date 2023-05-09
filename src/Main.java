01dba9f30ea0bd949bd24a444df2b89ac2772da4
amtenuJava <java.amanuel@gmail.com>
2023-05-09 16:47:24 -0600

    Item class and test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Item coke = new Item("drink", "coke", 1.5);
+        coke.printItem();
+        coke.setSize("small");
+        coke.printItem();
 
 
+        Item fries=new Item("topping","fries",1);
+        fries.printItem();
+        fries.setSize("Medium");
+        fries.printItem();
     }
 }
