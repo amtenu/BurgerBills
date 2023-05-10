9c43933e3055bfdb14fc917b9eecfae309fa4be4
amtenuJava <java.amanuel@gmail.com>
2023-05-10 12:34:26 -0600

    formatting

--- src/Item.java
+++ src/Item.java
@@ -2,7 +2,7 @@ public class Item {
     private String type;
     private String name;
     private double price;
-    private String size="MEDIUM";
+    private String size = "MEDIUM";
 
     public Item(String type, String name, double price) {
         this.type = type.toUpperCase();
@@ -11,38 +11,39 @@ public class Item {
     }
 
     public String getName() {
-        if(type.equals("SIDE") || type.equals("DRINK")){
+        if (type.equals("SIDE") || type.equals("DRINK")) {
             return size + " " + name;
         }
         return name;
     }
 
     public double getBasePrice() {
+
         return price;
     }
 
-    public double getAdjustedPrice(){
-        return switch(size.toUpperCase()){
-            case "SMALL" -> getBasePrice()- 0.5;
-            case "LARGE" ->getBasePrice() + 1;
+    public double getAdjustedPrice() {
+        return switch (size.toUpperCase()) {
+            case "SMALL" -> getBasePrice() - 0.5;
+            case "LARGE" -> getBasePrice() + 1;
             default -> getBasePrice();
 
         };
     }
 
     public void setSize(String size) {
+
         this.size = size.toUpperCase();
     }
 
-    public static void printItem(String name,double price){
-        System.out.printf("%20s:%6.2f%n",name.toUpperCase(),price);
+    public static void printItem(String name, double price) {
+        System.out.printf("%20s:%6.2f%n", name.toUpperCase(), price);
 
     }
 
-    public void printItem(){
-        printItem(getName(),getAdjustedPrice());
+    public void printItem() {
+        printItem(getName(), getAdjustedPrice());
     }
-
 
 
 }
