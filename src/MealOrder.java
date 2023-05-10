23c7851797ab9240f4448e5d7f4ff425a611a1d2
amtenuJava <java.amanuel@gmail.com>
2023-05-10 14:51:01 -0600

    check if the burger is regular or special

--- src/MealOrder.java
+++ src/MealOrder.java
@@ -13,6 +13,9 @@ public class MealOrder {
         } else {
 
         this.burger=new Burger(burgerType,4.0);
+        }
+
+
         this.drink=new Item("drink",drinkType,1.00);
         this.side=new Item("side",sideType,1.50);
     }
