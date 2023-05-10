1e4572a35cd83661a14657eadc082a3e591a3ea2
amtenuJava <java.amanuel@gmail.com>
2023-05-10 12:37:47 -0600

    added mealorder class and test on main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        Item coke = new Item("drink", "coke", 1.5);
-        coke.printItem();
-        coke.setSize("small");
-        coke.printItem();
+//        Item coke = new Item("drink", "coke", 1.5);
+//        coke.printItem();
+//        coke.setSize("small");
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
 
 
-        Item fries=new Item("topping","fries",1);
-        fries.printItem();
-        fries.setSize("Medium");
-        fries.printItem();
 
-        Burger burger=new Burger("regular",4.00);
-        burger.addTopping("Bacon","fries","");
-        burger.printItem();
+
+//        Item fries=new Item("topping","fries",1);
+//        fries.printItem();
+//        fries.setSize("Medium");
+//        fries.printItem();
+//
+//        Burger burger=new Burger("regular",4.00);
+//        burger.addTopping("Bacon","Ham","MAYO");
+//        burger.printItem();
+
+        MealOrder regularMeal=new MealOrder();
+//        regularMeal.printItemizedList();
+        regularMeal.addBurgetToppings("BACON","BACON","");
+        regularMeal.setDrinkSize("Large");
+        regularMeal.printItemizedList();
+
+
+
     }
 
 
