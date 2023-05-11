8c52bc006f6d8d784ffdca6e87244b40806516d9
amtenuJava <java.amanuel@gmail.com>
2023-05-11 05:14:44 -0600

    adjusted to check if burger is special or regular

--- src/MealOrder.java
+++ src/MealOrder.java
@@ -11,29 +11,40 @@ public class MealOrder {
         if(burgerType.equalsIgnoreCase("special")){//user can use capital or small
             this.burger=new SpecialBurger(burgerType,8.0);
         } else {
-
         this.burger=new Burger(burgerType,4.0);
         }
-
-
         this.drink=new Item("drink",drinkType,1.00);
         this.side=new Item("side",sideType,1.50);
     }
 
     public double getTotalPrice(){
+        if(burger instanceof SpecialBurger ){
+            return burger.getAdjustedPrice();
+        }
         return side.getAdjustedPrice()+drink.getAdjustedPrice()+ burger.getAdjustedPrice();
     }
 
     public void printItemizedList(){
         burger.printItem();
-        drink.printItem();
-        side.printItem();
+        if(burger instanceof SpecialBurger){
+            Item.printItem(drink.getName(),0);
+            Item.printItem(side.getName(),0);
 
+        } else {
+            drink.printItem();
+            side.printItem();
+        }
         System.out.println("-".repeat(30));
         Item.printItem("Total Price",getTotalPrice());
     }
 
-    public void addBurgetToppings(String extra1,String extra2, String extra3){
+    public void addBurgerToppings(String extra1,String extra2, String extra3){
+        burger.addTopping(extra1,extra2,extra3);
+    }
+    public void addBurgerToppings(String extra1,String extra2, String extra3,String extra4,String extra5){
+       if(burger instanceof SpecialBurger SBurger){
+           SBurger.addTopping(extra1,extra2,extra3,extra4,extra5);
+       }
         burger.addTopping(extra1,extra2,extra3);
     }
 
